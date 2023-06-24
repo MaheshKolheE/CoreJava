379a1bfe8390c9afdbf32665025a1e837d0e6179
mahesh <mahesh.kolhe717@gmail.com>
2023-06-24 17:44:35 +0530

    oddeven commit

---
+++ okgit/src/com/simple/oddEven.java
@@ -0,0 +1,13 @@
+package com.simple;
+
+public class oddEven {
+	public static void main(String[] args) {
+		int n=23;
+		if(n%2==0)
+			System.out.println("even");
+		else 
+			System.out.println("odd");
+		
+	}
+
+}
